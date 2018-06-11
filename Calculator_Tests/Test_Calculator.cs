aa654375d60fb2cc9ae2db5549862d135c55b050
Andrew Talecki <atalecki@villanova.edu>
2018-06-10 20:36:32 -0400

    Forst Commit

---
+++ Calculator_Tests/Test_Calculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator_Tests
+{
+    [TestClass]
+    public class Test_Calculator
+    {
+        [TestMethod]
+        public void Test_Addition()
+        {
+            double lhs = 3.1;
+            double rhs = 2.9;
+            Logic_Interfaces.IBinaryOperation addition = new Logic.Addition();
+            double result = addition.Perform_binary_calculation(lhs, rhs);
+            Assert.AreEqual(6.0, result);
+        }
+
+        [TestMethod]
+        public void Test_Divide()
+        {
+            double lhs = 12.0;
+            double rhs = 3.0;
+            Logic_Interfaces.IBinaryOperation division = new Logic.Division();
+            double result = division.Perform_binary_calculation(lhs, rhs);
+            Assert.AreEqual(4.0, result);
+        }
+
+        [TestMethod]
+        public void Test_Subtraction()
+        {
+            double lhs = 100.00;
+            double rhs = 50.0;
+            Logic_Interfaces.IBinaryOperation subtraction = new Logic.Subtraction();
+            double result = subtraction.Perform_binary_calculation(lhs, rhs);
+            Assert.AreEqual(50.0, result);
+        }
+
+        [TestMethod]
+        public void Test_Multiplication()
+        {
+            double lhs = 12.0;
+            double rhs = 12.0;
+            Logic_Interfaces.IBinaryOperation multiply = new Logic.Multiplication();
+            double result = multiply.Perform_binary_calculation(lhs, rhs);
+            Assert.AreEqual(144.0, result);
+        }
+
+        [TestMethod]
+        public void Test_Sqrt()
+        {
+            double input = 64.00;
+            Logic_Interfaces.IBinaryOperation sqrt = new Logic.SqareRoot();
+            double result = sqrt.Perform_binary_calculation(input, 0.0);
+            Assert.AreEqual(8.0, result);
+        }
+
+        [TestMethod]
+        public void Test_Recipricol()
+        {
+            double input = 5.0;
+            Logic_Interfaces.IBinaryOperation recip = new Logic.Reciprocal();
+            double result = recip.Perform_binary_calculation(input, 0.0);
+            Assert.AreEqual(.2, result);
+        }
+    }
+}
---
+++ Logic/Calculator.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic_Interfaces;
+
+namespace Logic
+{
+    public class Calculator
+    {
+        private IBinaryOperation pending_operation = null;
+        private double input, lhs, rhs;
+
+        public void Equals()
+        {
+            rhs = input;
+            if (pending_operation != null)
+            {
+                Console.WriteLine("Equals: " + pending_operation.Perform_binary_calculation(lhs, rhs));
+                pending_operation = null;
+                lhs = 0.0;
+                rhs = 0.0;
+                input = 0.0;
+            }
+            else
+            {
+                Console.WriteLine("You select an operation to perform. [+|-|*|/|reciprocal|square_root]");
+            }
+        }
+
+        public string Number_entered(double value)
+        {
+            input = value;
+            return value.ToString();
+        }
+
+        public string Addition_entered()
+        {
+            lhs = input;
+            pending_operation = new Addition();
+            return "Addition Selected [" + lhs + "]";
+        }
+
+        public string Subtraction_entered()
+        {
+            lhs = input;
+            pending_operation = new Subtraction();
+            return "Subtraction Selected [" + lhs + "]";
+        }
+
+        public string Multiplication_entered()
+        {
+            lhs = input;
+            pending_operation = new Multiplication();
+            return "Multiplication Selected [" + lhs + "]";
+        }
+
+        public string Division_entered()
+        {
+            lhs = input;
+            pending_operation = new Division();
+            return "Division Selected [" + lhs + "]";
+        }
+
+        public string Reciprocal_entered()
+        {
+            lhs = input;
+            pending_operation = new Reciprocal();
+            return "Reciprocal Selected [" + lhs + "]";
+        }
+
+        public string Square_root_entered()
+        {
+            lhs = input;
+            pending_operation = new SqareRoot();
+            return "Square Root Select [" + lhs + "]";
+        }
+    }
+
+    public class Addition : Logic_Interfaces.IBinaryOperation
+    {
+        public double Perform_binary_calculation(double lhs, double rhs)
+        {
+            double sum = lhs + rhs;
+            if (sum > double.MaxValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (sum < double.MinValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return sum;
+            }
+        }
+    }
+    public class Subtraction : Logic_Interfaces.IBinaryOperation
+    {
+        public double Perform_binary_calculation(double lhs, double rhs)
+        {
+            double difference = lhs - rhs;
+            if (difference > double.MaxValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (difference < double.MinValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return difference;
+            }
+        }
+    }
+    public class Multiplication : Logic_Interfaces.IBinaryOperation
+    {
+        public double Perform_binary_calculation(double lhs, double rhs)
+        {
+            double product = lhs * rhs;
+            if (product > double.MaxValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (product < double.MinValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return product;
+            }
+        }
+    }
+    public class Division : Logic_Interfaces.IBinaryOperation
+    {
+        public double Perform_binary_calculation(double lhs, double rhs)
+        {
+            double quotient = lhs / rhs;
+            if (quotient > double.MaxValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (quotient < double.MinValue)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return quotient;
+            }
+        }
+    }
+    public class Reciprocal : Logic_Interfaces.IBinaryOperation
+    {
+        public double Perform_binary_calculation(double lhs, double rhs)
+        {
+            return 1 / lhs;
+        }
+    }
+
+    public class SqareRoot : Logic_Interfaces.IBinaryOperation
+    {
+        public double Perform_binary_calculation(double lhs, double rhs)
+        {
+            return Math.Sqrt(lhs);
+        }
+    }
+}
